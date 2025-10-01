34a2aa9e9ff61b7db1dd1c5383d657543bcdfbad
tio-r <guntoroloa@gmail.com>
2025-10-01 09:27:18 +0700

    Route Dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view ('welcome');
 });
 
 Route::get('/daftar', function () {
     return view('daftar');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
 });
